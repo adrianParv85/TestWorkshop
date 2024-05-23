ad4be0275ff995c081dc88ecbd7d9ada0a3cac2a
adrianParv85 <adrian.parv85@gmail.com>
2024-05-23 16:22:18 +0300

    second Commit

--- src/test1.java
+++ src/test1.java
@@ -1,9 +1,70 @@
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+           System.out.println("Hello World");
 
-        System.out.println("Hello World");
+           problema1();
+         //  problema2();
+        //   problema3();
+        //   problema4();
+        //   problema5();
+         //  problema6();
+         //  problema7();
+        //   problema8();
+         //  problema9();
+        //   problema10();
+        //   problema11();
+         //  problema12();
+         //  problema13();
+        //   problema14();
+         //  problema15();
 
+
+
+    }
+
+    public static void problema1() {
+
+    }
+    public static void problema2() {
+
+    }
+    public static void problema3() {
+
+    }
+    public static void problema4() {
+
+    }
+    public static void problema5() {
+
+    }
+    public static void problema6() {
+
+    }
+    public static void problema7() {
+
+    }
+    public static void problema8() {
+
+    }
+    public static void problema9() {
+
+    }
+    public static void problema10() {
+
+    }
+    public static void problema11() {
+
+    }
+    public static void problema12() {
+
+    }
+    public static void problema13() {
+
+    }
+    public static void problema14() {
+
+    }
+    public static void problema15() {
 
     }
 }
